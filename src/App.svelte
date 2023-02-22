3ccedb20222dde10abb3107c88ff1684ef760db6
Max Wilson <maxjw98@gmail.com>
2023-02-22 14:01:28 +0000

    Added password protection

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,55 @@
 <script lang="ts">
     import Quotes from './components/Quotes.svelte';
     import { SvelteToast } from '@zerodevx/svelte-toast';
+
+    let password = '';
+    const hash = (s) =>
+        s.split('').reduce((a, b) => {
+            a = (a << 5) - a + b.charCodeAt(0);
+            return a & a;
+        }, 0);
 </script>
 
-<Quotes />
+{#if hash(password) === 1252560117}
+    <Quotes />
+{:else}
+    <div id="password-container">
+        <label for="password">Password:</label>
+        <input id="password" bind:value={password} type="password" />
+    </div>
+{/if}
+
 <SvelteToast />
+
+<style>
+    #password-container {
+        position: fixed;
+        display: flex;
+        flex-direction: column;
+        max-width: fit-content;
+        justify-content: center;
+        margin: 0 auto;
+        height: 100vh;
+        top: 0;
+        left: 0;
+        right: 0;
+        z-index: 1;
+        text-align: center;
+    }
+
+    #password-container label {
+        font-size: 2.5rem;
+        font-weight: 500;
+        margin-top: 5px;
+        margin-bottom: 10px;
+        color: white;
+        cursor: default;
+    }
+
+    #password {
+        margin: 0 auto;
+        margin-top: 10px;
+        width: 500px;
+        max-width: 90vw;
+    }
+</style>

59ed837263f797b13b9bf095266167243cc90a7d
Max Wilson <maxjw98@gmail.com>
2023-07-14 22:20:43 +0100

    Added optional password

--- src/App.svelte
+++ src/App.svelte
@@ -18,14 +18,24 @@
                 console.log(error.code, error.message);
             });
     }
+
+    let password = '';
+    const hash = (s) =>
+        s.split('').reduce((a, b) => {
+            a = (a << 5) - a + b.charCodeAt(0);
+            return a & a;
+        }, 0);
 </script>
 
-{#if user}
+{#if !user || hash(password) === 1252560117}
     <Quotes />
 {:else}
     <div id="login-container">
         <button on:click={login} id="login" class="custom-button"> Sign In with Google </button>
-        <p>(This means no more typing in a password)</p>
+        <p>(This means no more typing in a password each time)</p>
+        <h1>OR</h1>
+        <label for="password">Password:</label>
+        <input id="password" bind:value={password} type="password" />
     </div>
 {/if}
 
@@ -52,6 +62,22 @@
     }
 
     #login {
+        margin: 0 auto;
+        margin-top: 10px;
+        width: 500px;
+        max-width: 90vw;
+    }
+
+    #login-container label {
+        font-size: 2.5rem;
+        font-weight: 500;
+        margin-top: 5px;
+        margin-bottom: 10px;
+        color: white;
+        cursor: default;
+    }
+
+    #password {
         margin: 0 auto;
         margin-top: 10px;
         width: 500px;
